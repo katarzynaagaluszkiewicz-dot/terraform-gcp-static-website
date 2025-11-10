2faad089a5cd97fbbeb5ca2f600aa3f502441b91
katarzynaagaluszkiewicz-dot <katarzynaa.galuszkiewicz@gmail.com>
2025-11-10 20:44:12 +0100

    variables.tf

--- variables.tf
+++ variables.tf
@@ -10,9 +10,3 @@ variable "region" {
   type        = string
   default     = "us-central1"
 }
-
-variable "bucket_name_prefix" {
-  description = "Prefix for the bucket name"
-  type        = string
-  default     = "static-site"
-}
